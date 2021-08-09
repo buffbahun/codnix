828e1681584b3846126318ee59846857bcba3640
buffbahun <88264839+buffbahun@users.noreply.github.com>
2021-08-09 16:33:12 +0545

    Add files via upload

--- office/script.js
+++ office/script.js
@@ -1,18 +1,57 @@
-let one = document.querySelector('.sub_nav .sb ul');
-let bg = document.querySelector('.sub_nav');
-let mde = document.querySelector('header>nav>.media');
-let mnu = document.querySelector('nav>.logo_nav>.nav>ul');
+let nav = document.querySelectorAll('.sb>ul>li>span');
+let snav = document.querySelectorAll('.sub_nav>.sub>ul');
+let bg = document.querySelector('header .sub_nav');
 
-function dis(evt){
+function show(evt) {
 
-    [...one.children].forEach(el => {
-        el.classList.remove('disp');
+
+    
+    [...snav].forEach(ell => {
+
+        ell.className = "";
+    
+    });  
+    
+    [...nav].forEach(ele => {
+
+        ele.parentNode.className = "";
+
     });
-    bg.className = 'sub_nav';
-    bg.classList.add(evt.target.className);
+    
+    bg.classList.remove("one");
+    bg.classList.remove("two");
+    bg.classList.remove("three");
 
-    evt.target.classList.add('disp');
+    if(evt.target.className === "one"){
+
+        snav[0].classList.add('one');
+        nav[0].parentNode.classList.add('one');
+        bg.classList.add('one');
+
+    }
+
+    if(evt.target.className === "two"){
+
+        snav[1].classList.add('two');
+        nav[1].parentNode.classList.add('two');
+        bg.classList.add('two');
+
+    }
+
+    if(evt.target.className === "three"){
+
+        snav[2].classList.add('three');
+        nav[2].parentNode.classList.add('three');
+        bg.classList.add('three');
+        
+    }
+
 
 }
 
-one.addEventListener('click',dis,false);
+
+[...nav].forEach(el => {
+
+    el.addEventListener('mouseover',show,false);
+
+});
